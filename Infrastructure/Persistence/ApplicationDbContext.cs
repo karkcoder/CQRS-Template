552bb2e1e60cf3bb849762094e819c4448d779c9
Tim Karki <urkarki@gmail.com>
2022-01-31 21:11:22 -0700

    Added product feature

---
+++ Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using Duende.IdentityServer.EntityFramework.Options;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Infrastructure.Persistence
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions)
+            : base(options, operationalStoreOptions)
+        {
+
+        }
+        public DbSet<Product> Products => Set<Product>();
+    }
+}
